5dc9318b9dfdff9624376c821b6dabde806c0dbc
jbofill10 <juan.bofill10@gmail.com>
2020-08-22 21:43:38 -0400

    Add mcts template

---
+++ mcts.py
@@ -0,0 +1,19 @@
+class MonteCarloTreeSearch:
+
+    def __init__(self):
+        pass
+
+    def choose(self):
+        pass
+
+    def rollout(self):
+        pass
+
+    def expand(self):
+        pass
+
+    def simulate(self):
+        pass
+
+    def back_propagate(self):
+        pass
