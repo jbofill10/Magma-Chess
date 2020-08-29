13c4c9e20cca35fa2251b38f1da4b954d0950437
jbofill10 <juan.bofill10@gmail.com>
2020-08-29 19:23:25 -0400

    add query by index function

--- db.py
+++ db.py
@@ -1,9 +1,10 @@
 import psycopg2
-import dotenv, os, sys
+import dotenv, os
 dotenv.load_dotenv()
+import numpy
 
 
-class SQL:
+class PSQL:
 
     connection = None
     cursor = None
@@ -37,4 +38,51 @@ class SQL:
             except Exception as e:
                 print(f'Error inserting into PostgreSQL\n\n{e}')
                 print("INSERT INTO train(id, position, result) VALUES ({}, '{}', '{}');".format(index, position, result))
-                sys.exit(0)
+
+    def get_row_count(self):
+        with self.connection.cursor() as cursor:
+            try:
+                cursor.execute(
+                    "SELECT COUNT(*) FROM train;"
+                )
+
+                row_count = cursor.fetchone()
+                print("Row count successfully queried")
+                print(row_count)
+                return row_count[0]
+
+            except Exception as e:
+                print("Not able to get row count!")
+                print(e)
+
+    def read_records_at(self, idxs):
+        with self.connection.cursor() as cursor:
+            try:
+                cursor.execute(
+                    f'SELECT position, result FROM train where id >= {idxs[0]} and id <= {idxs[len(idxs)-1]}'
+                )
+
+                query_data = cursor.fetchall()
+                x = []
+                y = []
+                for i in query_data:
+                    x.append(i[0])
+                    temp_y = i[1]
+
+                    if temp_y == '1':
+                        y.append([1, 0, 0])
+
+                    elif temp_y == '1/2-1/2':
+                        y.append([0, 1, 0])
+
+                    else:
+                        y.append([0, 0, 1])
+
+                x = numpy.array(x)
+                y = numpy.array(y)
+                return x.reshape(len(x), 8,9,1), y
+
+            except Exception as e:
+                print('Unable to retrieve training batch!')
+                print(e)
+
