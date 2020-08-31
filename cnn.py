c18b1fa89a5c6f646604839476ddabddb87de534
jbofill10 <juan.bofill10@gmail.com>
2020-08-30 23:25:02 -0400

    tweak architecture and batch size

--- cnn.py
+++ cnn.py
@@ -1,33 +1,38 @@
+import os, pickle
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
 from datagen import DataGenerator
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
-
-import os, pickle
+from tensorflow.keras.layers import Conv2D, Dense, Flatten
 
 
 def train():
+
     model = Sequential([
-        Conv2D(filters=8, kernel_size=3, activation='relu', input_shape=(8,9,1)),
+        Conv2D(filters=8, kernel_size=3, activation='relu', input_shape=(8, 9, 1)),
         # MaxPooling2D((2, 2)),
         Conv2D(filters=16, kernel_size=3, activation='relu'),
         # MaxPooling2D((2, 2)),
         Conv2D(filters=32, kernel_size=3, activation='relu'),
-        # MaxPooling2D((2, 2)),
-        Conv2D(filters=64, kernel_size=2, activation='relu'),
+        Conv2D(filters=128, kernel_size=2, activation='relu'),
         Flatten(),
         Dense(3, activation='softmax')
 
     ])
+
+    callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=6)
 
     model.compile(
         optimizer='adam',
         loss='categorical_crossentropy',
         metrics=['accuracy']
     )
-    
+
     model.summary()
 
-    history = model.fit(DataGenerator(batch_size=9120), verbose=1)
+    history = model.fit(DataGenerator(batch_size=25), verbose=1, epochs=100, callbacks=callback)
 
     if not os.path.isfile('Data/model/model_results'):
         model_hist = history.history
