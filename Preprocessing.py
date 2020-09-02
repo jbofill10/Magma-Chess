79870dee1552e3792471446955d4c8ab2565272c
jbofill10 <juan.bofill10@gmail.com>
2020-09-02 00:30:35 -0400

    add threading support for parsing games

--- Preprocessing.py
+++ Preprocessing.py
@@ -7,20 +7,39 @@ import chess.pgn
 import io
 import pprint
 import sys
+import threading
 
 
 def run(df):
 
-    sql = PSQL()
     df.drop(['White', 'Black'], inplace=True, axis=1)
 
     df['Result'] = df['Result'].replace({'1-0': 1, '1/2-1/2': 0, '0-1': -1})
+    df = df[df['Result'].isin([1,0,-1])]
 
+    split = len(df)//4
+
+    df1 = df[0:split]
+    df2 = df[split:(2*split)]
+    df3 = df[2*split:(3*split)]
+    df4 = df[3*split::]
+
+    t1 = threading.Thread(target=parse_games, kwargs=dict(df=df1))
+    t2 = threading.Thread(target=parse_games, kwargs=dict(df=df2))
+    t3 = threading.Thread(target=parse_games, kwargs=dict(df=df3))
+    t4 = threading.Thread(target=parse_games, kwargs=dict(df=df4))
+
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    # parse_games(df)
+
+
+def parse_games(df):
+    sql = PSQL(list_size=len(df))
     # Tracks game result
     count = 0
-    # Count of each position
-    index = 0
-
     board = chess.Board()
     for i in tqdm(df['gamePGN']):
         result = df['Result'].iloc[count]
@@ -63,41 +82,48 @@ def run(df):
 
                 print(e)
 
-                sys.exit(0)
             b = convert_to_int(board, white_kcastle, black_kcastle, white_qcastle, black_qcastle)
 
-            sql.add_position(index, str(b.tolist()).replace("[", "{").replace("]", "}").replace(".0", ""), result)
-
-            index += 1
+            sql.add_position(str(b.tolist()).replace("[", "{").replace("]", "}").replace(".0", ""), result)
 
             move_counter += 1
         count += 1
 
 
 def convert_to_int(board, white_kcastle, black_kcastle, white_qcastle, black_qcastle):
-    l = [None] * 64
+    white_pos = [None] * 64
+    black_pos = [None] * 64
     for sq in chess.scan_reversed(board.occupied_co[chess.WHITE]):  # Check if white
-        l[sq] = board.piece_type_at(sq)
+        white_pos[sq] = board.piece_type_at(sq)
     for sq in chess.scan_reversed(board.occupied_co[chess.BLACK]):  # Check if black
-        l[sq] = -board.piece_type_at(sq)
-    board_map = [0 if v is None else v for v in l]
+        black_pos[sq] = board.piece_type_at(sq)
+    white_map = [0 if v is None else v for v in white_pos]
+    black_map = [0 if v is None else v for v in black_pos]
 
-    matrix = np.zeros((2, 8, 8))
+    matrix = np.zeros((7, 8, 8))
 
     counter = 0
     for i in range(len(matrix[0])):
         for j in range(len(matrix[0][0])):
-            matrix[0][i][j] = board_map[counter]
+            matrix[0][i][j] = white_map[counter]
+            matrix[3][i][j] = black_map[counter]
 
             counter += 1
 
     # Turn
-    matrix[1][0][0] = 12 if board.turn else -12
+    if board.turn:
+        matrix[6].fill(0)
+    else:
+        matrix[6].fill(1)
 
     # Check for castling rights
-    matrix[1][6][0] = 1 if white_kcastle else 0
-    matrix[1][6][0] = 1 if black_kcastle else 0
-    matrix[1][7][7] = 1 if white_qcastle else 0
-    matrix[1][7][7] = 1 if black_qcastle else 0
+    if white_kcastle:
+        matrix[1].fill(1)
+    if white_qcastle:
+        matrix[2].fill(1)
+    if black_kcastle:
+        matrix[4].fill(1)
+    if black_qcastle:
+        matrix[5].fill(1)
 
     return matrix
--- db.py
+++ db.py
@@ -10,10 +10,12 @@ class PSQL:
     cursor = None
     commit_count = 0
 
-    def __init__(self):
+    def __init__(self, list_size=None):
         try:
             self.connection = psycopg2.connect(os.getenv('SQL_URL'))
             self.cursor = self.connection.cursor()
+            self.list_size = list_size
+            self.total_commits = 0
 
             print('Connected to PostgreSQL!')
 
@@ -21,75 +23,72 @@ class PSQL:
 
             print(f'Could not connect to PostgreSQL\n\n{e}')
 
-    def add_position(self, index, position, result):
-
+    def add_position(self, position, result):
         with self.connection.cursor() as cursor:
 
             try:
                 cursor.execute(
-                    "INSERT INTO train(id, position, result) VALUES ({}, '{}', '{}');".format(index, position, result)
+                    "INSERT INTO train(position, result) VALUES ('{}', {});".format(position, result)
                 )
 
                 self.commit_count += 1
+                self.total_commits += 1
+
                 if self.commit_count == 100000:
                     self.commit_count = 0
                     self.connection.commit()
 
+                if self.total_commits == self.list_size:
+                    self.connection.commit()
+
             except Exception as e:
                 print(f'Error inserting into PostgreSQL\n\n{e}')
-                print("INSERT INTO train(id, position, result) VALUES ({}, '{}', '{}');".format(index, position, result))
+                print("INSERT INTO train(position, result) VALUES ('{}', {});".format(position, result))
 
     def get_row_count(self):
         with self.connection.cursor() as cursor:
-            try:
-                # cursor.execute(
-                #     "SELECT COUNT(*) FROM train;"
-                # )
-                #
-                # row_count = cursor.fetchone()
-                # print("Row count successfully queried")
-                # print(row_count)
-                # return row_count[0]
-                return 225300000
+            # try:
+            #     cursor.execute(
+            #         "SELECT COUNT(*) FROM train;"
+            #     )
+            #
+            #     row_count = cursor.fetchone()
+            #     print("Row count successfully queried")
+            #     print(row_count)
+            #     return row_count[0]
+            #
+            # except Exception as e:
+            #     print("Not able to get row count!")
+            #     print(e)
 
-            except Exception as e:
-                print("Not able to get row count!")
-                print(e)
+            return 225300000
 
     def read_records_at(self, idxs):
         with self.connection.cursor() as cursor:
             try:
-                # cursor.execute(
-                #     f'SELECT position, result FROM train where id >= {idxs[0]} and id <= {idxs[len(idxs)-1]}'
-                # )
-                #
-                # query_data = cursor.fetchall()
-                # x = []
-                # y = []
-                # for i in query_data:
-                #     x.append(i[0])
-                #     temp_y = i[1]
-                #
-                #     if temp_y == '1':
-                #         y.append([1, 0, 0])
-                #
-                #     elif temp_y == '1/2-1/2':
-                #         y.append([0, 1, 0])
-                #
-                #     else:
-                #         y.append([0, 0, 1])
-                #
-                # x = numpy.array(x)
-                # y = numpy.array(y)
-                # return x.reshape(len(x), 8,9,1), y
+                cursor.execute(
+                    f'SELECT position, result FROM train where id >= {idxs[0]} and id <= {idxs[len(idxs)-1]}'
+                )
 
-                x = [[[[4,2,3,5,6,3,2,4],[1,1,1,1,0,1,1,1],[0,0,0,0,0,0,0,0],[0,0,0,0,1,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,-1,0,0,0],[-1,-1,-1,-1,0,-1,-1,-1],[-4,-2,-3,-5,-6,-3,-2,-4]],
-                     [[4,2,3,5,6,3,2,4],[1,1,1,1,0,1,1,1],[0,0,0,0,0,0,0,0],[0,0,0,0,1,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,-1,0,0,0],[-1,-1,-1,-1,0,-1,-1,-1],[-4,-2,-3,-5,-6,-3,-2,-4]]]]
-                y = [[1,0,0]]
+                query_data = cursor.fetchall()
+                x = []
+                y = []
+                for i in query_data:
+                    x.append(i[0])
+                    temp_y = i[1]
+
+                    if temp_y == '1':
+                        y.append([1, 0, 0])
+
+                    elif temp_y == '1/2-1/2':
+                        y.append([0, 1, 0])
+
+                    else:
+                        y.append([0, 0, 1])
+
                 x = numpy.array(x)
-                # x = x.reshape(len(x), (8,8,2))
-                print(x.shape[1:])
-                return x, numpy.array(y)
+                y = numpy.array(y)
+                return x, y
 
             except Exception as e:
                 print('Unable to retrieve training batch!')
